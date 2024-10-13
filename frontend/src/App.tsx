3b1e963c3f95fe45f114fe3935bfaf6b00e85ca2
arif013 <mdarif11099@gmail.com>
2024-10-14 00:06:04 +0530

    file processing added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Typewriter from './components/Typewriter';
@@ -8,29 +8,54 @@ export default function TextGenerator() {
   const [prompt, setPrompt] = useState('');
   const [generatedText, setGeneratedText] = useState('');
   const [loading, setLoading] = useState(false);
+  const [file, setFile] = useState<File | null>(null)
+  const [ fileName, setFileName ] = useState('No file is there')
 
+  // Function to set file 
+  function handleFileChange(e: ChangeEvent<HTMLInputElement>): void {
+    const files = e.target.files
+    if (files && files.length > 0) {
+      setFile(files[0])
+      setFileName(files[0].name)
+    }
+  }
 
-  const handleGenerateText = async () => {
+  // Uploading file to the backend 
+  async function handleFileUpload() {
+
     setLoading(true)
     try {
-      // const response = await fetch('https://ai-generator-py1d.onrender.com/api/data', {
-        const response = await fetch('http://localhost:3000/api/data', {
+      const data = new FormData()
+      if (!file){
+        data.append('prompt', JSON.stringify(prompt))
+        console.log(prompt)
+      } 
+      else {
+        data.append(`file`, file)
+        data.append('prompt', JSON.stringify( prompt ))
+        console.log(file)
+      }
+
+
+      const response = await fetch('http://localhost:3000/api/upload', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ prompt })
-      });
+        body: data
+      })
+
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
       const result = await response.json();
       setGeneratedText(result.response); // Access the generated text properly
+
     } catch (error) {
-      console.error('Error: ', error);
+      console.log(error)
     }
+
+    // .then(data => console.log(data))
+    // .catch(err => console.log(err))
     setLoading(false)
-  };
+  }
 
   return (
     <div className="min-h-screen bg-gray-100">
@@ -39,53 +64,68 @@ export default function TextGenerator() {
           <h1 className="text-3xl font-bold text-gray-900 "> AI Text Generator</h1>
         </div>
         <div className="border-b border-gray-200 pb-5 mb-5 sticky top-0">
-            <div className="mt-3 sm:mt-0 sm:ml-4">
-              <label htmlFor="prompt" className="sr-only">
-                Enter your prompt
-              </label>
-              <div className="flex rounded-md shadow-sm max-w-7xl mx-auto">
-                <Input
-                  type="text"
-                  name="prompt"
-                  id="prompt"
-                  className="flex-1 block w-full rounded-none rounded-l-md sm:text-sm border-gray-300"
-                  placeholder="Enter your prompt here"
-                  value={prompt}
-                  onChange={(e) => setPrompt(e.target.value)}
-                  onKeyDown={e => {
-                    if (e.key === 'Enter') {
-                      e.preventDefault(); // Prevent default action
-                      handleGenerateText(); // Call the function on Enter key press
-                      console.log("Enter pressed");
-                    }
-                  }}
+          <div className="mt-3 sm:mt-0 sm:ml-4">
+            <label htmlFor="prompt" className="sr-only">
+              Enter your prompt
+            </label>
+            <div className="flex rounded-md shadow-sm">
+              <Input
+                type="text"
+                name="prompt"
+                id="prompt"
+                className="flex-1 block w-full rounded-none rounded-l-md sm:text-sm border-gray-300"
+                placeholder="Enter your prompt here"
+                value={prompt}
+                onChange={(e) => setPrompt(e.target.value)}
+                onKeyDown={e => {
+                  if (e.key === 'Enter') {
+                    e.preventDefault(); // Prevent default action
+                    // handleGenerateText(); // Call the function on Enter key press
+                    handleFileUpload()
+                  }
+                }}
+              />
+
+              {/* Input and button to upload file */}
+              {/* <input type="file" onChange={handleFileChange} /> */}
+              <label htmlFor="file-upload" className="cursor-pointer inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-none text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <span>Upload file</span>
+                <input
+                  id="file-upload"
+                  name="file-upload"
+                  type="file"
+                  className="sr-only"
+                  onChange={handleFileChange}
                 />
-               <Button
+              </label>
+              <Button
                 type="button"
-                onClick={handleGenerateText}
+                onClick={handleFileUpload}
                 className="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
               >
                 Generate
               </Button>
-
-              </div>
+              {/* {file} */}
             </div>
           </div>
-      </header>
-      <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div className="px-4  sm:px-0">
+          <p className='flex justify-end mx-10 my-2 text-xs' > File Added:  {fileName}</p>
           
+        </div>
+      </header>
+      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div className="px-4 py-6 sm:px-0">
+
           <div className="mt-6 bg-white shadow px-4 py-5 sm:rounded-lg sm:p-6 ">
             <h2 className="text-lg font-medium text-gray-900 mb-4">Generated Text</h2>
             <div className="bg-gray-50 p-4 rounded-md">
               {loading ? (
                 // {/* <pre className="text-gray-700 text-wrap">{generatedText || "Your generated text will appear here."}</pre> */}
-                <Loader type="spinner-circle" bgColor='#6b6767' color='#6b6767'title={"loading..."} size={60} />
-              ):
-              (
-                <Typewriter text={generatedText || "Your generated text will appear here."} delay={10}/>  
-              )
-            }
+                <Loader type="spinner-circle" bgColor='#6b6767' color='#6b6767' title={"loading..."} size={60} />
+              ) :
+                (
+                  <Typewriter text={generatedText || "Your generated text will appear here."} delay={10} />
+                )
+              }
               {/* <div 
                 className="text-gray-700" 
                 dangerouslySetInnerHTML={{ __html: generatedText || "Your generated text will appear here." }} 
--- index.js
+++ index.js
@@ -4,40 +4,183 @@ const PORT = 3000;
 const app = express();
 require('dotenv').config();
 const cors = require('cors')
+const upload = require('./src/middlewares/upload.js')
+const { readFile } = require('fs/promises'); // Use fs/promises for async file reading
+
+// import { GoogleAIFileManager } from "@google/generative-ai/server";
+const { GoogleAIFileManager } = require("@google/generative-ai/files")
+
+
+
 
 app.use(cors())
 
 // Initialize the generative model
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+// const { readFile } = require('fs');
 
 // Access your API key as an environment variable
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
+// Initialize GoogleAIFileManager with your API_KEY.
+const fileManager = new GoogleAIFileManager(process.env.API_KEY);
+
+
 // Use body-parser middleware to parse JSON request bodies
 app.use(bodyParser.json());
 
-app.post('/api/data', async (req, res) => {
-  const { prompt } = req.body; // Get the prompt from the request body
+// For text-only input, use the gemini-pro model
+// const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-  if (!prompt) {
-    return res.status(400).json({ error: 'Prompt is required' });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+
+
+// app.post('/api/data', async (req, res) => {
+//   const { prompt } = req.body; // Get the prompt from the request body
+
+//   if (!prompt) {
+//     return res.status(400).json({ error: 'Prompt is required' });
+//   }
+
+//   try {
+
+//     const result = await model.generateContent(prompt);
+//     const response = await result.response;
+//     const text = response.text();
+//     console.log(text);
+
+//     res.json({ response: text });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Something went wrong' });
+//   }
+// });
+
+// app.post('/api/upload', upload.single('file'), (req,res)=>{
+//   res.status(204).end()
+//   readFile(req.file.path)
+//   .then(jsonObj => console.log(jsonObj))
+// })
+
+// app.post('/api/upload', upload.single('file'), async (req, res) => {
+//   const prompt  = req.body.prompt;
+//   console.log(req.file)
+//   if (!req.file && !prompt) {
+//     return res.status(400).json({ error: 'No file uploaded and no prompt' });
+
+//   } else if (!req.file && !!prompt) {
+//     try {
+
+//       const result = await model.generateContent(prompt);
+//       const response = await result.response;
+//       const text = response.text();
+//       console.log(text);
+
+//       // res.json({ response: text });
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'Something went wrong' });
+//     }
+//   } else {
+//     try {
+//       const filePath = req.file.path; // Ensure you have the correct path to the uploaded file
+//       const fileData = await readFile(filePath, 'utf8'); // Read the file asynchronously
+//       // Process the file data as needed here...
+//       res.status(200).json({ message: 'File uploaded successfully' }); // Send success response
+//       // Upload the file and specify a display name.
+//       const uploadResponse = await fileManager.uploadFile(filePath, {
+//         mimeType: "application/pdf",
+//         displayName: "Gemini 1.5 PDF",
+//       });
+
+//       // View the response.
+//       console.log(
+//         `Uploaded file ${uploadResponse.file.displayName} as: ${uploadResponse.file.uri}`,
+//       );
+//       // Generate content using text and the URI reference for the uploaded file.
+//       const result = await model.generateContent([
+//         {
+//           fileData: {
+//             mimeType: uploadResponse.file.mimeType,
+//             fileUri: uploadResponse.file.uri,
+//           },
+//         },
+//         { text:prompt },
+//       ]);
+
+//       // Output the generated text to the console
+//       const response = result.response
+//       const text =  response.text()
+//       console.log(text)
+//       res.json({ response: text })
+//       // console.log(result.response.text());
+
+//     } catch (error) {
+//       console.error('File upload error:', error);
+//       res.status(500).json({ error: 'Error processing the file' });
+//     }
+//   }
+
+
+// });
+
+app.post('/api/upload', upload.single('file'), async (req, res) => {
+  const prompt = req.body.prompt;
+  // console.log(req.file);
+  let responseText; // Variable to hold the response text
+
+  if (!req.file && !prompt) {
+    return res.status(400).json({ error: 'No file uploaded and no prompt' });
   }
 
   try {
-    // For text-only input, use the gemini-pro model
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-    
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-    console.log(text);
+    // If no file is uploaded, just process the prompt
+    if (!req.file && !!prompt) {
+      const result = await model.generateContent(prompt);
+      const response = await result.response;
+      responseText = response.text();
+    } else {
+      // Process the uploaded file
+      const filePath = req.file.path; // Ensure you have the correct path to the uploaded file
+      const fileData = await readFile(filePath, 'utf8'); // Read the file asynchronously
 
-    res.json({ response: text });
+      // Upload the file and specify a display name
+      const uploadResponse = await fileManager.uploadFile(filePath, {
+        mimeType: "application/pdf",
+        displayName: "Gemini 1.5 PDF",
+      });
+
+      console.log(
+        `Uploaded file ${uploadResponse.file.displayName} as: ${uploadResponse.file.uri}`
+      );
+
+      // Generate content using the uploaded file's URI and the prompt
+      const result = await model.generateContent([
+        {
+          fileData: {
+            mimeType: uploadResponse.file.mimeType,
+            fileUri: uploadResponse.file.uri,
+          },
+        },
+        { text: prompt }, // Use the actual prompt here
+      ]);
+
+      // Get the response text
+      const response = result.response;
+      responseText = response.text();
+    }
+
+    // Send the response after processing is done
+    res.json({ response: responseText });
+
   } catch (error) {
-    console.error(error);
+    console.error('Error:', error);
     res.status(500).json({ error: 'Something went wrong' });
   }
 });
+
+
 
 app.get('/', (req, res) => {
   res.send('Hi');
